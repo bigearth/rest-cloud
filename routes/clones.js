aeea7345db0be33578aa722722b2d82f36fe4ef3
Gabriel Cardona <gabriel.cardona@bluerocket.us>
2017-06-01 21:27:56 -0700

    Trying it out.

--- routes/clones.js
+++ routes/clones.js
@@ -10,4 +10,16 @@ router.get('/:id/health-check', (req, res, next) => {
   res.send('healthy and happy');
 });
 
+router.get('/:id/mqtt', (req, res, next) => {
+  let mqtt = require('mqtt');
+  var client = mqtt.connect('mqtts://jqpcdchr:scRzM1YSc4kh@m13.cloudmqtt.com:10805');
+  client.on('connect', function() {
+    client.publish('t1', new Date().toString(), function() {
+      client.end();
+      res.writeHead(204, { 'Connection': 'keep-alive' });
+      res.end();
+    });
+  });
+});
+
 module.exports = router;
